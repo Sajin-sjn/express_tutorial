981e08c9b0168042ce56abd0dc4f29783757da16
Sajin PJ <sajinsjn4433@gmail.com>
2024-09-13 16:43:31 +0530

    submitted datas from signup form are fetched and printed

--- sample.js
+++ sample.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 let app = express()
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, 'home.html'))
 })
@@ -12,7 +14,19 @@ app.get('/signup',(req,res) => {
 })
 
 app.post('/signup',(req,res) => {
-    res.sendFile(path.join(__dirname, 'sign_up.html'))
+    // const name = req.body.name
+    // const place = req.body.place
+    // const username = req.body.username;
+    // const password = req.body.password store the datas seperately
+
+    const {name,place,username,password} = req.body
+
+    console.log('Name:', name);
+    console.log('Place:', place);
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    res.send('Form data received and printed in the console!');
 })
 
 app.listen(400, () => console.log('server started'))
