48b87ea88e915b6df689473c1bf924b95b38d86c
Sajin PJ <sajinsjn4433@gmail.com>
2024-09-13 16:04:29 +0530

    created a home page, when clicked sign up link navigate to sign up page

--- sample.js
+++ sample.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const path = require('path')
 
 let app = express()
 
 app.get('/',(req,res) => {
-    res.send("hello world, this is in the default url that is home...")
+    res.sendFile(path.join(__dirname, 'home.html'))
+})
+
+app.get('/signup',(req,res) => {
+    res.sendFile(path.join(__dirname, 'sign_up.html'))
+})
+
+app.post('/signup',(req,res) => {
+    res.sendFile(path.join(__dirname, 'sign_up.html'))
 })
 
 app.listen(400, () => console.log('server started'))
